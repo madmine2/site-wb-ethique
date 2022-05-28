2d15a52695868a8f489ec28b8643b819e918cc6a
madmine2 <87324175+madmine2@users.noreply.github.com>
2022-05-28 21:54:01 +0200

    image mouvement ethique des droits

--- animation.js
+++ animation.js
@@ -12,8 +12,23 @@ sr.reveal('#image1', {
     origin: 'top',
     distance:'50px',
     scale:1,
-    duration: 1200,
+    duration: 1400,
     reset: false, 
 });
 
+sr.reveal('#titre1', {
+    origin: 'top',
+    distance:'80px',
+    scale:1,
+    duration: 1400,
+    reset: false, 
+});
+
+sr.reveal('#titre2', {
+    origin: 'top',
+    distance:'50px',
+    scale:1.1,
+    duration: 1200,
+    reset: false, 
+});
 
